2170fe58f94745599b8284ddcba3ee66a1bddecb
Tom <94thartman@gmail.com>
2022-11-26 13:01:06 -0500

    done for the day

--- web/components/common/DesktopNav.tsx
+++ web/components/common/DesktopNav.tsx
@@ -6,14 +6,14 @@ import Link from 'next/link'
 const Nav = tw.nav`relative flex flex-col items-center`
 const Top = tw.ul`flex flex-row uppercase font-poppins text-lg`
 const Children = styled.ul`
-  ${tw`bg-grey absolute bottom-0 left-0 flex-col hidden text-white translate-y-full`}
+  ${tw`bg-grey absolute bottom-0 left-0 flex-col hidden min-w-full text-white translate-y-full before:([content:""] bg-primary w-full absolute top-0 left-0 -translate-y-full h-0.5)`}
   a {
     ${tw`w-full py-1 pl-1 pr-6`}
   }
 `
 const TopItem = styled.li(() => [
-  tw`px-3 py-1 leading-none relative after:([content:''] absolute bottom-0 mx-auto left-0 right-0 bg-primary h-0.5 w-0 duration-500 ease-in-out)`,
-  tw`hover:(after:w-full [.children]:flex)`,
+  tw`[& > a]:(px-3 py-1) flex leading-none relative after:([content:''] absolute bottom-0 mx-auto left-0 right-0 bg-primary h-0.5 w-0 duration-500 ease-in-out)`,
+  tw`hover:(after:w-[calc(100% - 1.25rem)] [.children]:flex)`,
 ])
 const ChildItem = styled.li`
   ${tw`hover:bg-secondary flex w-full text-base capitalize`}
--- web/components/common/Header.tsx
+++ web/components/common/Header.tsx
@@ -6,7 +6,7 @@ import styled from 'styled-components'
 import DesktopNav from '@common/DesktopNav'
 
 const Component = styled.header(() => [
-  tw`border-offBlack border-opacity-30 fixed top-0 left-0 flex flex-row items-center justify-between w-full gap-4 px-2 py-2 duration-300 ease-in-out border-b shadow-xl`,
+  tw`border-offBlack border-opacity-30 fixed top-0 left-0 flex flex-row items-center justify-between w-full gap-4 px-4 py-2 duration-300 ease-in-out border-b shadow-xl`,
 ])
 const Number = tw.a`text-xl font-poppins hover:text-primary duration-500 ease-in-out`
 
