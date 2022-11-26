47e96e811e275c3553b2f5864032dc21295f1757
Tom <94thartman@gmail.com>
2022-11-26 12:22:44 -0500

    fixed stupid crap

--- web/components/common/DesktopNav.tsx
+++ web/components/common/DesktopNav.tsx
@@ -1,17 +1,22 @@
 import type { VFC } from 'react'
 import tw from 'twin.macro'
 import styled, { css } from 'styled-components'
-import NextLink from 'next/link'
+import Link from 'next/link'
 
 const Nav = tw.nav`relative flex flex-col items-center`
 const Top = tw.ul`flex flex-row uppercase font-poppins text-lg`
-const Children = tw.ul`absolute bg-grey text-white flex-col bottom-0 translate-y-full hidden`
-const TopLink = styled(NextLink)(() => [
+const Children = styled.ul`
+  ${tw`bg-grey absolute bottom-0 left-0 flex-col hidden text-white translate-y-full`}
+  a {
+    ${tw`w-full py-1 pl-1 pr-6`}
+  }
+`
+const TopItem = styled.li(() => [
   tw`px-3 py-1 leading-none relative after:([content:''] absolute bottom-0 mx-auto left-0 right-0 bg-primary h-0.5 w-0 duration-500 ease-in-out)`,
-  tw`hover:(after:w-full)`,
+  tw`hover:(after:w-full [.children]:flex)`,
 ])
-const ChildLink = styled(NextLink)`
-  ${tw`hover:bg-secondary w-full py-1 pl-1 pr-4 text-base capitalize`}
+const ChildItem = styled.li`
+  ${tw`hover:bg-secondary flex w-full text-base capitalize`}
 `
 
 interface FilterProps {}
@@ -65,15 +70,15 @@ const navItems = [
 
 const DesktopNav: VFC<FilterProps> = ({ ...rest }) => {
   const listItems = (nav, child = false) => {
-    const Link = !!child ? ChildLink : TopLink
+    const Item = !!child ? ChildItem : TopItem
     const List = !!child ? Children : Top
     return (
       <List className={!!child ? 'children' : ''}>
         {nav.map(({ title, url, children }, i) => (
-          <li key={i} tw="relative flex hover:[.children]:flex">
+          <Item key={i}>
             <Link href={url}>{title}</Link>
             {!!children && children.length > 0 && listItems(children, true)}
-          </li>
+          </Item>
         ))}
       </List>
     )
