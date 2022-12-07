64c997b8198d56e5b057c72064cfced0a0ccd065
rjhartman <rhartman1239@gmail.com>
2022-12-06 23:16:38 -0800

    Set up data fetching from Sanity

--- web/components/frontPage/FrontHero.tsx
+++ web/components/frontPage/FrontHero.tsx
@@ -1,16 +1,21 @@
-import type { VFC } from 'react'
+import type { FC } from 'react'
 import tw from 'twin.macro'
 import Image from 'next/image'
 import hero from '../../public/hero.jpg'
 
-const Section = tw.section`w-full max-w-full h-screen [img]:(object-cover brightness-50) overflow-hidden`
+const Section = tw.section`w-full max-w-full h-screen text-white font-bold text-4xl [img]:(object-cover brightness-[25%] absolute z-behind) flex flex-col items-center justify-center overflow-hidden`
 
-interface FilterProps {}
+interface FrontHeroProps {
+  title: 'string'
+  subtitle: 'string'
+}
 
-const FrontHero: VFC<FilterProps> = ({ ...rest }) => {
+const FrontHero: FC<FrontHeroProps> = ({ title, subtitle, ...rest }) => {
   return (
     <Section {...rest}>
       <Image src={hero} alt="hero image" fill />
+      <h1 tw="text-7xl">{title}</h1>
+      <h3 tw="text-4xl text-primary mt-2">{subtitle}</h3>
     </Section>
   )
 }
---
+++ web/pages/index.js
@@ -0,0 +1,38 @@
+import sanityClient from '@sanity/client'
+import tw from 'twin.macro'
+import Layout from '@common/Layout'
+import FrontHero from 'components/frontPage/FrontHero'
+import groq from 'groq'
+
+const configuredSanityClient = sanityClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  useCdn: true,
+  apiVersion: '2021-10-21',
+})
+
+const Div = tw.div`h-[130vh] w-full bg-pink-300`
+export default function Home({ data }) {
+  return (
+    <Layout>
+      <FrontHero title={data.hero.title} subtitle={data.hero.subtitle} />
+      <Div />
+    </Layout>
+  )
+}
+
+export const getServerSideProps = async function () {
+  // Is there a better way to type `data` here?
+  // We'd have to somehow eject the type defs from Sanity and
+  // tell the client that's what we expect to be returned.
+  // That's not real type safety, and defeats the purpose of using Typescript at all.
+  // Made this a js file for now, to kick the can down the road and decide if it's worth it.
+  const [data] = await configuredSanityClient.fetch(groq`
+    *[_type == 'homePage'] { hero }
+  `)
+  return {
+    props: {
+      data,
+    },
+  }
+}
