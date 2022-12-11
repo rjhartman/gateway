d75510fd3c93512435772bdaad64bd8b6c9c4147
rjhartman <rhartman1239@gmail.com>
2022-12-10 18:54:28 -0800

    Fix CI

--- web/components/frontPage/FrontHero.tsx
+++ web/components/frontPage/FrontHero.tsx
@@ -1,11 +1,16 @@
 import type { FC } from 'react'
 import tw from 'twin.macro'
 import Image from '@common/SanityImage'
-import { FrontHero } from 'lib/schema'
+import { FrontHero as FrontHeroType } from 'lib/schema'
 
 const Section = tw.section`w-full max-w-full h-screen text-white font-bold text-4xl flex flex-col items-center justify-center overflow-hidden`
 
-const FrontHero: FC<FrontHero> = ({ title, subtitle, background, ...rest }) => {
+const FrontHero: FC<FrontHeroType> = ({
+  title,
+  subtitle,
+  background,
+  ...rest
+}) => {
   return (
     <Section {...rest}>
       <Image
